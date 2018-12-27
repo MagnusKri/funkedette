adc84326989bcfb242eca27a7629c3917a12655d
Magnuskri <magnus9555@gmail.com>
2018-12-27 16:14:01 +0100

    la til noe avanserte regnestykker

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // la til kommentar
+        int lol=1+0+1;
+        System.out.println("Hello World!" + lol); // la til kommentar
+
     }
 }
